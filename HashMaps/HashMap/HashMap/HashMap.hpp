ffd45e063dc2c5f8647a3de7d588f3dcd11c0f1f
lishanlung <cnjlwitter@gmail.com>
2025-06-23 02:51:50 +0100

    created cpp hashmap sln

---
+++ HashMaps/HashMap/HashMap/HashMap.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <hash>
+
+using namespace std;
+
+template<typename K, typename V>
+class HashMap
+{
+	vector vec;
+	size_t size;
+
+public:
+	HashMap();
+
+};
+
---
+++ HashMaps/HashMap/HashMap/tests.cpp
@@ -0,0 +1,12 @@
+// tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "HashMap.hpp"
+
+using namespace std;
+
+int main()
+{
+    
+}
