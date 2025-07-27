ae9f1e6f2e55b1f2703c03c31b4eb472728786c4
lishanlung <cnjlwitter@gmail.com>
2025-07-27 02:38:46 +0100

    Implemented project 2 :D

--- Arrays/Projects/Slicing Window Maximum Visualised/Slicing Window Maximum Visualised/Slicing Window Maximum Visualised.cpp
+++ Arrays/Projects/Slicing Window Maximum Visualised/Slicing Window Maximum Visualised/Slicing Window Maximum Visualised.cpp
@@ -1,4 +1,4 @@
-// Slicing Window Maximum Visualised.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Slicing Window Maximum Visualised.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -40,13 +40,62 @@ void printVec(vector<T>& vec, int window_start = 0, int k = 0) {
 }
 
 
-vector<int> MaxSlicingWindow(vector<int> vec, int k) {
+vector<int> maxSlicingWindow(vector<int> vec, int k) {
     vector<int> output;
+    int max, start, end;
+
+    for (int i = 0; i < vec.size() - k + 1; i++) {
+        max = 0;
+        start = i;
+        end = i + k;
+
+        cout << "Iteration " << i + 1 << endl << "-----------" << endl;
+        printVec(vec, start, k);
+        
+        cout << endl << "max(";
+
+        for (int j = start; j < end; j++) {
+            
+            cout << vec[j];
+
+            if (j < end - 1) {
+                cout << ", ";
+            }
+
+            if (vec[j] > max) {
+                max = vec[j];
+            }
+        }
+
+        cout << ") = " << max << endl;
+        
+        output.push_back(max);
+        
+        cout << "output = ";
+        printVec(output);
+        cout << endl << endl;
+    }
 
     return output;
 }
 
 int main()
 {
+    //vars
     vector<int> vec = { 3, 6, 3, 7, 3, 5, 2, 4 };
+    int k = 3;
+
+    //paragraph
+    cout << "--- MAX SLICING WINDOW ALGORITHM VISUALISED ---" << endl << endl;
+    cout << "You are given an array of integers nums and an integer k. ";
+    cout << "There is a sliding window of size k that starts at the left edge of the array ";
+    cout << "The window slides one position to the right until it reaches the right edge of the array. " << endl << endl;
+    cout << "Return a list that contains the maximum element in the window at each step." << endl << endl;
+    cout << "----------------------------------------" << endl << "Array = ";
+    printVec(vec);
+    cout << " | k = " << k << endl;
+    cout << "{x, y, z} => current window" << endl << "----------------------------------------" << endl << endl;
+
+    //computation
+    maxSlicingWindow(vec, k);
 }
